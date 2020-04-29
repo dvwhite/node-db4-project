2e4c3ac13ac390732f651c3519e2a7f312f1c223
David White <dwhite0627@gmail.com>
2020-04-28 23:27:56 -0400

    Add getRecipesWithIngredient db helper function

--- routes/recipes/recipeDb.js
+++ routes/recipes/recipeDb.js
@@ -4,7 +4,8 @@ module.exports = {
   getRecipes,
   getRecipeById,
   getShoppingList,
-  getInstructions
+  getInstructions,
+  getRecipesWithIngredient
 };
 
 function getRecipes() {
@@ -30,4 +31,12 @@ function getInstructions(recipe_id) {
     .leftJoin("recipe_instructions as ri", "r.id", "ri.recipe_id")
     .leftJoin("instructions as i", "i.id", "ri.instruction_id")
     .orderBy("i.number")
+}
+
+function getRecipesWithIngredient(ingredient_id) {
+  return db("recipes as r")
+    .select(["r.name"])
+    .leftJoin("recipe_ingredients as ri", "r.id", "ri.recipe_id")
+    .leftJoin("ingredients as i", "i.id", "ri.ingredient_id")
+    .where("i.id", ingredient_id)
 }
