7a30c4ba5b13fb75acc45f9a26af8b02cae8339e
David White <dwhite0627@gmail.com>
2020-04-28 23:28:23 -0400

    Add whitespace to error middleware in server.js

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ server.use("/api/recipes", recipeRoutes)
 server.use("/api/ingredients", ingredientRoutes)
 
 // Error middleware
-// server.use((err, req, res, next) => {
-//   console.log(err)
-//   if (err) {
-//     return res.status(500).json({
-//       message: "There was an error performing the required operation",
-//       validation: [],
-//       data: {}
-//     });
-//   }
-// })
+server.use((err, req, res, next) => {
+  console.log(err)
+  if (err) {
+    return res.status(500).json({
+      message: "There was an error performing the required operation",
+      validation: [],
+      data: {}
+    });
+  }
+})
 
 module.exports = server;
